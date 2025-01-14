af4b8633c345f5a125a37fe6dc475e7bae37c8c8
Ahmed Frag <a.frag166@gmail.com>
2025-01-14 08:16:58 +0200

    Handling Form Submission & (Managing & Getting User Input via State & Generic Handlers)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,72 @@
+import { useState } from "react";
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState('')
+  // const [enteredPassword, setEnteredPassword] = useState('')
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(enteredValues);
+  };
+
+  const handleInputChange = (identifier, value) => {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
+  };
+
+  // const handleEmailChange = (event) => {
+  //   setEnteredEmail(event.target.value);
+  // };
+
+  // const handlePasswordChange = (event) => {
+  //   setEnteredPassword(event.target.value);
+  // };
+
+  const handleReset = () => {
+    setEnteredValues((prevValues) => ({
+      email: "",
+      password: "",
+    }));
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={enteredValues.email}
+            onChange={(event) => handleInputChange("email", event.target.value)}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={enteredValues.password}
+            onChange={(event) => handleInputChange("password", event.target.value)}
+          />
         </div>
       </div>
 
       <p className="form-actions">
-        <button className="button button-flat">Reset</button>
+        <button className="button button-flat" onClick={handleReset}>
+          Reset
+        </button>
         <button className="button">Login</button>
       </p>
     </form>
